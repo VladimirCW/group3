8e013b0c56ff16fba5736937597c416fcc5971d9
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2020-02-28 20:11:48 +0200

    initial commit

---
+++ src/test/java/tests/Unit.java
@@ -0,0 +1,13 @@
+package test.java.tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class Unit {
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+}
